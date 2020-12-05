f06e25a2614f51c3984991bc037ebbdacf841b89
Tim Sawicki <tsawicki@redhat.com>
2020-12-04 20:20:41 -0500

    remove data line

--- day-04/day4/p2.py
+++ day-04/day4/p2.py
@@ -1,7 +1,5 @@
 from day4.helpers import *
 
-# data = cleaned_data(get_raw_data('/home/tsawicki/gitlab/truthordata/advent-of-code-2020/day-04/day4/'))
-
 if __name__ == '__main__':
     print(len(get_valid_records(
         cleaned_data(get_raw_data()),
